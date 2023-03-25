7696377d9b93536815ec8cd9f17bedb036f8910d
sorens-it <sorens.it@gmail.com>
2023-03-25 15:37:32 -0700

    ab

--- sublimelyyours/js/style.js
+++ sublimelyyours/js/style.js
@@ -12,3 +12,15 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+}
